82ff3aee70595857f3d25d1e2d145a5e2ee12242
Santiago Montoya <smontoyadev@gmail.com>
2022-06-20 20:57:07 -0500

    Cambios en los comentarios para mayor claridad

--- src/block.js
+++ src/block.js
@@ -2,16 +2,24 @@ const { SHA256 } = require("crypto-js");
 
 class Block {
     constructor(blockIndex, blockCreation, blockTx, blockPreviousHash){
-        this.blockIndex = blockIndex;
-        this.blockCreation = blockCreation;
-        this.blockTx = blockTx;
-        this.blockPreviousHash = blockPreviousHash;
-        this.hash = this.getHash();
+        this.blockIndex = blockIndex; // La posición del bloque en la cadena
+        this.blockCreation = blockCreation; // timestamp
+        this.blockTx = blockTx; // Transacciones y data
+        this.blockPreviousHash = blockPreviousHash; // Hash del bloque anterior para poder conectarlos
+        this.hash = this.getHash(); // Convierte toda la info del bloque en un hash
+        this.nonce = nonce; // El número usado para el PoW
+       // this.merkleRoot = this.getMerkleRoot(); *** Método pendiente *** Investigar como implementar el árbol Merkle
     }
 
     getHash(){
         return SHA256(this.blockPreviousHash + this.blockCreation + JSON.stringify(this.blockTx))
     }
+
+    getMerkleRoot(){
+        
+    }
+
+
 }
 
 module.exports = Block;
--- src/blockchain.js
+++ src/blockchain.js
@@ -1,6 +1,9 @@
 const Block = require("./block");
 
 class Blockchain{
+
+    //En la clase blockchain debería ir el bloque génesis?***DUDA
+
     constructor(){
         //this.chain = []
       
